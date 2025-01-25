0f7826f17ad8093de8c167253672edf8efb9fa71
laxmi kumari <kajalprajapati7451@gmail.com>
2025-01-25 09:44:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("Welcome to Tic Tac Toe");
+
 let music = new Audio("magic-spell-shoot-sound-effect-236816.mp3");
 let audioTurn = new Audio("tiktokio-com-vh7obneep33mtcsbfkxn-65462.mp3");
 let gameover = new Audio("ding-36029 (1).mp3");
 let turn = "X";
+let isgameover = false;
 
 // Function to change the turn
 const changeTurn = () => {
@@ -11,21 +13,58 @@ const changeTurn = () => {
 
 // Function to check for a win
 const checkWin = () => {
+    let boxText = document.getElementsByClassName('boxText');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    wins.forEach(e => {
+        if (
+            (boxText[e[0]].innerText === boxText[e[1]].innerText) &&
+            (boxText[e[1]].innerText === boxText[e[2]].innerText) &&
+            (boxText[e[0]].innerText !== "")
+        ) {
+            document.querySelector('.info').innerText = boxText[e[0]].innerText + " Won!";
+            isgameover = true;
+            gameover.play();
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "200px";
+        }
+    });
 };
 
 // Game Logic
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
-    let boxtext = element.querySelector('.boxtext');
+    let boxText = element.querySelector('.boxText');
     element.addEventListener('click', () => {
-        if (boxtext.innerText === '') {
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            audioTurn.play();
+        if (boxText.innerText === '' && !isgameover) {
+            boxText.innerText = turn;
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            if (!isgameover) {
+                turn = changeTurn();
+                audioTurn.play();
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     });
 });
 
-
+// Optional: Play background music manually
+document.getElementById('reset').addEventListener('click', () => {
+    let boxTexts = document.querySelectorAll('.boxText');
+    Array.from(boxTexts).forEach(element => {
+        element.innerText = '';
+    });
+    turn = "X";
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "0px";
+    gameover.pause();
+    gameover.currentTime = 0;
+});
